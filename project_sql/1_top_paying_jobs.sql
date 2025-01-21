909a3f259bd83440be7a09823b57092f8f2ae563
Abhiram-who <abhiramwho@gmail.com>
2025-01-22 02:39:44 +0530

    message

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,26 @@
+/*
+This query is to find the top paying data scientist jobs
+-Incorporates top 10 highest paying roles.
+-With specified salaries (while removing nulls). 
+-Highlights the best oppurtunities
+*/
+
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short= 'Data Scientist' AND
+    job_location= 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
